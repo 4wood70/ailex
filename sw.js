5bf7b0a1dc8a1e9417149f946158d60217862350
4wood70 <4wood70@gmail.com>
2026-06-24 18:39:30 +0200

    Some bugs fixed

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* AILEX service worker — офлайн-оболочка.
    Версию меняй при каждом обновлении, чтобы кэш обновился. */
-const CACHE = 'ailex-v42';
+const CACHE = 'ailex-v43';
 const SHELL = [
   './',
   './index.html',
